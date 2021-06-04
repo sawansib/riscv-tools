83adeed20fb39a648cc7f3490d3cfd48163c9fdd
Sawan Singh <singh.sawan@um.es>
2021-06-04 14:19:49 +0200

    Fixed some compilation issues

--- build.sh
+++ build.sh
@@ -16,10 +16,10 @@ check_version() {
 
 check_version automake 1.14 "OpenOCD build"
 check_version autoconf 2.64 "OpenOCD build"
-build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --enable-jtag_vpi --disable-werror
+CFLAGS="-fno-common" build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --enable-jtag_vpi --disable-werror
 
 build_project riscv-isa-sim --prefix=$RISCV
-CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv64-unknown-elf
+CC=/opt/riscv/bin/riscv64-unknown-linux-gnu-gcc CXX=/opt/riscv/bin/riscv64-unknown-linux-gnu-g++ OBJCOPY=/opt/riscv/bin/riscv64-unknown-linux-gnu-objcopy build_project riscv-pk --prefix=$RISCV --host=riscv64-unknown-elf
 build_project riscv-tests --prefix=$RISCV/riscv64-unknown-elf
 
 echo -e "\\nRISC-V Toolchain installation completed!"
